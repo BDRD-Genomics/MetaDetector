30eb3231f94721c7be0a95bdfd5a8dfa1ff50b22
APaskey <64111239+APaskey@users.noreply.github.com>
2025-12-17 15:50:01 -0500

    Update download_taxdump.sh

--- databases/download_taxdump.sh
+++ databases/download_taxdump.sh
@@ -14,12 +14,13 @@ conda activate md
 
 md_db_dir=/path/to/databases
 
-# Redownload this to update every once in awhile
+# Redownload this to update every once in a while
 mkdir $md_db_dir/taxdump_$(date +%d%b%y) && ln -sf $md_db_dir/taxdump_$(date +%d%b%y) $md_db_dir/taxdump
 wget -cvb -O $md_db_dir/taxdump/nucl_gb.accession2taxid.gz https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz #(or most recent nucl taxid)
 wget -cvb -O $md_db_dir/taxdump/prot.accession2taxid.gz https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.gz #(or most recent prot taxid)
 cd $md_db_dir/taxdump/
 pigz -dc nucl_gb.accession2taxid.gz > nucl_gb.accession2taxid
 pigz -dc nucl_gb.accession2taxid.gz > prot.accession2taxid
+
 
 
